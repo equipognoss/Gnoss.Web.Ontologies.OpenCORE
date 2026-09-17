d10ebb460b628d267f1258b93324bcc925ddb744
Javier-RSP <javierruiz@gnoss.com>
2026-09-17 16:14:38 +0200

    Security Improvement

--- Gnoss.Web.Ontologies/Program.cs
+++ Gnoss.Web.Ontologies/Program.cs
@@ -4,7 +4,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Serilog;
 using System;
-
+using Microsoft.AspNetCore.Builder;
 namespace Gnoss.Web.Ontologies
 {
     public class Program
